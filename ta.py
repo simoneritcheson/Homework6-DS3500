8f80e940e573875ac584e806f060efc62236708b
allypicerno <alisonpicerno@gmail.com>
2024-03-28 11:03:10 -0400

    allys vchanges: underwilling & main

--- ta.py
+++ ta.py
@@ -1,23 +1,32 @@
 import pandas as pd
 import numpy as np
 import pytest
+import evo
 
 tas_df = pd.read_csv('tas.csv')
 sections_df = pd.read_csv('sections.csv')
+section_prefs = tas_df.loc[:, '0']
 
 # Process TA Data
 # Assume columns 0 to 16 in tas_df correspond to availability and preference
 ta_data = tas_df.iloc[:, 3:].replace({'U': 0, 'W': 1, 'P': 2}).values
 
+
 # we still need max_assigned information for each TA:
 def max_assigned(tas_df):
     return tas_df['max_assigned'].values
+
+
 max_assigned = max_assigned(tas_df)
+
 
 # Isolate time information for each practicum section
 def times(sections_df):
     return sections_df['daytime'].values
+
+
 times = times(sections_df)
+
 
 # Process Section Data
 def section_data(sections_df):
@@ -26,6 +35,7 @@ def section_data(sections_df):
     max_ta = sections_df['max_ta'].values
     # Have baby arrays in big array [min, max]
     return np.column_stack((min_ta, max_ta))
+
 
 section_data = section_data(sections_df)
 # Combine Data
@@ -39,11 +49,12 @@ data_arrays = [np.random.randint(2, size=num_sections) for _ in range(num_tas)]
 
 # Combine the individual arrays into a single data structure
 data = np.array(data_arrays)
+print(data)
+
 
 # before we do objectives/agents, function should assign ta's to section.
 # loop through different lists within the list, assign a random number from the TA options
 # start by randomly assigning them to a practicum with no consideration for their preferences
-
 
 
 # Add objectives below
@@ -59,7 +70,9 @@ def overallocation(data, max_assigned):
     :return: Total penalty value.
     """
 
-    return sum([num_assigned - max_assigned[i] for i, num_assigned in enumerate(map(sum, data)) if num_assigned > max_assigned[i]])
+    return sum([num_assigned - max_assigned[i] for i, num_assigned in enumerate(map(sum, data)) if
+                num_assigned > max_assigned[i]])
+
 
 def undersupport(data, section_data):
     """
@@ -77,9 +90,10 @@ def undersupport(data, section_data):
     # get penalty scores by matching up corresponding indeces in the num_ones_per_column and section_data arrays
     # only consider a penalty score when the value in num_ones_per_column is less than the first element in the inner array of section_data
     penalties = sum([section_value - num_ones if num_ones < section_value else 0 for num_ones, section_value in
-                 zip(num_ones_per_column, section_data[:, 0])]) # section_data[:, 0] selects all the min_amounts
+                     zip(num_ones_per_column, section_data[:, 0])])  # section_data[:, 0] selects all the min_amounts
 
     return penalties
+
 
 def time_conflict(data, times):
     """
@@ -91,15 +105,33 @@ ADD DOCSTRING
     penalty = 0
     for i in range(len(data)):
         ta_array = data[i]
-        indices = np.where(ta_array==1)[0]
-        #print('this is indeces: ', indices)
+        indices = np.where(ta_array == 1)[0]
+        # print('this is indeces: ', indices)
         selected_times = times[indices]
         # Check if there's a conflict 
         has_conflict = len(selected_times) != len(np.unique(selected_times))
-        #print("Is there a time conflict?", has_conflict)
+        # print("Is there a time conflict?", has_conflict)
         if has_conflict:
             penalty += 1
     return penalty
 
 
+def unwilling(data):
+    unwilling_lst = [] = np.where((data == 1) & section_prefs == 'U', 1, 0)
+    unwilling_count = [sum(lst) for lst in unwilling_lst]
+    return sum(unwilling_count)
 
+
+def main():
+    # create the environment
+    E = evo.Environment()
+
+    # register the fitness functions
+    E.add_fitness_criteria("unwilling", unwilling)
+
+    data = np.array(data_arrays)
+    print(data)
+
+
+if __name__ == '__main__':
+    main()
